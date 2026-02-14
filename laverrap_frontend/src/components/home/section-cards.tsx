dc7908beb7a7e38371bfd430fddcbd37f3a2264f
GonzaloHaag <gonzalohaag13@gmail.com>
2026-02-14 17:11:32 -0300

    fix: Proyecto listo para produccion v1

--- laverrap_frontend/src/components/home/section-cards.tsx
+++ laverrap_frontend/src/components/home/section-cards.tsx
@@ -4,10 +4,10 @@ import { CarIcon, DollarSignIcon, Users2Icon } from "lucide-react";
 export const SectionCards = () => {
   return (
     <section className="container-cards">
-      <CardPage title="Ingresos del mes" value={245000} Icon={DollarSignIcon} isMoney />
-      <CardPage title="Lavados totales" value={342} Icon={CarIcon} />
-      <CardPage title="Clientes activos" value={25} Icon={Users2Icon} />
-      <CardPage title="Activos" value={150} Icon={DollarSignIcon} />
+      <CardPage title="Ingresos del mes" value={245000} Icon={DollarSignIcon} isLoading={false} isMoney />
+      <CardPage title="Lavados totales" value={342} Icon={CarIcon} isLoading={false} />
+      <CardPage title="Clientes activos" value={25} Icon={Users2Icon} isLoading={false} />
+      <CardPage title="Activos" value={150} Icon={DollarSignIcon} isLoading={false} />
     </section>
   );
 };
